2757d7652b72ecca9bb76d0891b2ec5d8b9801ba
ccsalazar <ccsalazar200@gmail.com>
2017-11-01 00:56:08 -0700

    feat: Add component for book shelf content

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import BookShelfContent from './BookShelfContent'
 
 
 class ListBooks extends Component {
@@ -11,9 +12,10 @@ class ListBooks extends Component {
   render(){
 
     const {books} = this.props;
-    let currentlyReading
-    let wantToRead
-    let finishedReading
+    let currentlyReading;
+    let wantToRead;
+    let finishedReading;
+
     currentlyReading = books.filter(book=>book.shelf==='currentlyReading');
     wantToRead = books.filter(book=>book.shelf==='wantToRead');
     finishedReading = books.filter(book=>book.shelf==='read');
@@ -28,34 +30,23 @@ class ListBooks extends Component {
             <div className="bookshelf">
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {currentlyReading.map((book)=>(
-                    <li key={book.id}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div
-                            className="book-cover"
-                            style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
-                  ))}
-                </ol>
+                <BookShelfContent shelfContent={currentlyReading}/>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <BookShelfContent shelfContent={wantToRead}/>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <BookShelfContent shelfContent={finishedReading}/>
               </div>
             </div>
           </div>
-        </div> {/* END OF LIST BOOKS CONTENT */}
+        </div>
         <div className="open-search">
           <Link to='/search' >Add a book</Link>
         </div>
